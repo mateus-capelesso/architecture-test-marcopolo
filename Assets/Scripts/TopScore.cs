a5b11e6ce68c8d32b968576432a5ae42ce90ac05
mateus.capelesso <mateus.capelesso@gmail.com>
2021-08-06 22:12:14 -0300

    Change game canvas to single scene

--- Assets/Scripts/TopScore.cs
+++ Assets/Scripts/TopScore.cs
@@ -3,10 +3,10 @@ using UnityEngine.UI;
 
 public class TopScore : MonoBehaviour
 {
-    public Text label;
+    private Text _label;
     
     private void Awake()
     {
-        label.text += GamePlay.Instance.GetScore().ToString();
+        GetComponent<Text>().text += GamePlay.Instance.GetScore().ToString();
     }
 }
