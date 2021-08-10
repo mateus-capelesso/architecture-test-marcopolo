075dfe5cf7a311b5d7ef6c34308adc75eea4e043
mateus.capelesso <mateus.capelesso@gmail.com>
2021-08-10 16:11:13 -0300

    Prevent paddle from bouncing

--- Assets/Scripts/Gameplay/PlayerController.cs
+++ Assets/Scripts/Gameplay/PlayerController.cs
@@ -1,11 +1,14 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 namespace Gameplay
 {
     public class PlayerController : MonoBehaviour
     {
+        private enum InputConstraints {Left, Right, None}
         [SerializeField] private float velocity = 2f;
         private Rigidbody2D _rigidbody;
+        [SerializeField] private InputConstraints _constraints = InputConstraints.None;
 
         private void Start()
         {
@@ -25,8 +28,45 @@ namespace Gameplay
     
         private void MovePlayer(float xValue)
         {
+            xValue = CheckConstraints(xValue);
             var direction = new Vector2(xValue, 0f);
             _rigidbody.velocity = direction * velocity;
+        }
+
+        private float CheckConstraints(float value)
+        {
+            if (_constraints == InputConstraints.Left)
+            {
+                if (value < 0f)
+                {
+                    value = 0f;
+                }
+                else
+                {
+                    _constraints = InputConstraints.None;
+                }
+            }
+            
+            if (_constraints == InputConstraints.Right)
+            {
+                if (value > 0f)
+                {
+                    value = 0f;
+                }
+                else
+                {
+                    _constraints = InputConstraints.None;
+                }
+            }
+
+            return value;
+        }
+
+        private void OnCollisionEnter2D(Collision2D other)
+        {
+            if (!other.transform.CompareTag("Wall")) return;
+
+            _constraints = transform.position.x > 0f ? InputConstraints.Right : InputConstraints.Left;
         }
     }
 }
